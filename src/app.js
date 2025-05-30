d81d84ead632f9a49bef0de4be2eb69860479818
Bittu Kumar <krbittu09278@gmail.com>
2025-05-30 16:24:32 +0530

    palying with next

--- src/app.js
+++ src/app.js
@@ -3,24 +3,26 @@ const express = require('express');
 const app = express();
 
 //this will match only GET requests
-app.get("/user",(req, res) => {
+app.get("/user/:id/:name",(req, res) => {
+    console.log(req.params);
     res.send({firstname: "John", lastname: "Doe"});
 });
 
-//this will match only POST requests
-app.post("/user", (req, res) => {
-    res.send("saved user information successfully");
-});
+// //this will match only POST requests
+// app.post("/user", (req, res) => {
+//     res.send("saved user information successfully");
+// });
 
-//this eill be use to delete the user
-app.delete("/user", (req, res) => {
-    res.send("deleted user information successfully");
-});
+// //this eill be use to delete the user
+// app.delete("/user", (req, res) => {
+//     res.send("deleted user information successfully");
+// });
 
-//this will match all the http methods
-app.use("/test", (req, res) => {
-    res.send("Hello from the server");
-});
+// //this will match all the http methods
+// app.use("/test", (req, res) => {
+//     res.send("Hello from the server");
+// });
+
 
 app.listen(7777, () => {
     console.log('Server is running on port 7777');
