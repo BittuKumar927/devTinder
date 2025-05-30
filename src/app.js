958fbcf4803ac707a9f115aa99da24e59d7746ef
Bittu Kumar <krbittu09278@gmail.com>
2025-05-30 17:47:36 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,27 +2,15 @@ const express = require('express');
 
 const app = express();
 
-//this will match only GET requests
-app.get("/user/:id/:name",(req, res) => {
-    console.log(req.params);
-    res.send({firstname: "John", lastname: "Doe"});
+app.use("/user", (req, res, next) => {
+    console.log("Handling the Route Handler 1");
+    res.send("Route Handler 1");
+    next();
+},(req, res, next) => {
+    //res.send("Route Handler 2");
+    console.log("Handling the Route Handler 2");
+    res.send("Route Handler 2");
 });
-
-// //this will match only POST requests
-// app.post("/user", (req, res) => {
-//     res.send("saved user information successfully");
-// });
-
-// //this eill be use to delete the user
-// app.delete("/user", (req, res) => {
-//     res.send("deleted user information successfully");
-// });
-
-// //this will match all the http methods
-// app.use("/test", (req, res) => {
-//     res.send("Hello from the server");
-// });
-
 
 app.listen(7777, () => {
     console.log('Server is running on port 7777');
