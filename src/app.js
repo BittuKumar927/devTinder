c919180c250df865b12fd7c62878ecee84e47eb4
Bittu Kumar <krbittu09278@gmail.com>
2025-05-30 15:54:36 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello From hello route");
+//this will match only GET requests
+app.get("/user",(req, res) => {
+    res.send({firstname: "John", lastname: "Doe"});
 });
 
+//this will match only POST requests
+app.post("/user", (req, res) => {
+    res.send("saved user information successfully");
+});
+
+//this eill be use to delete the user
+app.delete("/user", (req, res) => {
+    res.send("deleted user information successfully");
+});
+
+//this will match all the http methods
 app.use("/test", (req, res) => {
     res.send("Hello from the server");
 });
