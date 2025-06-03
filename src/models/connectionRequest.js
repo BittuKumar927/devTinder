ae584a706d278e3eee24a210c6f2ad8485d17459
Bittu Kumar <krbittu09278@gmail.com>
2025-06-03 16:59:34 +0530

    connectionrequestingnoredandinterested

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const connectionRequestSchema = new mongoose.Schema({
+    formUserId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    toUserId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['ignored', 'accepted', 'rejected', 'interested'],
+        message:`{VALUE} is not a valid status`,
+    }
+},{
+    timestamps: true
+});
+
+connectionRequestSchema.pre('save', function(next){
+    const connectionRequest = this;
+
+    //fromUserId and toUserId should not be the same
+    if (connectionRequest.formUserId.toString() === connectionRequest.toUserId.toString()) {
+        throw new Error("You cannot send a connection request to yourself");
+    }
+    next();// Call next to continue the save operation
+
+});
+
+
+const connectionRequestModel = new mongoose.model('ConnectionRequest', connectionRequestSchema);
+
+module.exports = connectionRequestModel;
+// This model defines the structure for connection requests in the database.
--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,18 +1,70 @@
 const express = require('express');
 const User = require('../models/user.js');
-const bcrypt = require('bcrypt'); // Import bcrypt for password hashing
 const {  userAuth } = require("../middlewares/auth.js"); // Import authentication middlewares
-const { validateSignUpData } = require('../utils/validation.js');
-const jwt = require('jsonwebtoken'); // Import jsonwebtoken for token handling
+const ConnectionRequest = require('../models/connectionRequest.js'); // Import the connection request model
 
-const requestgRouter = express.Router();
+const requestRouter = express.Router();
 
 //send connection request
-requestgRouter.post("/sendconnectionrequest", userAuth, async (req, res) => {
-    console.log("Connection request received");
+requestRouter.post("/request/send/:status/:touserId", userAuth, async (req, res) => {
+    try{
 
-    res.send("Connection request sent successfully");
+        const fromUserId = req.user._id; // Get the ID of the user making the request
+
+        const toUserId = req.params.touserId; // Get the ID of the user to whom the request is being sent
+
+        const  status = req.params.status; // Get the status of the request (e.g., 'interested')
+
+        const allowedStatuses = ['ignored', 'interested']; // Define allowed statuses
+
+        if(!allowedStatuses.includes(status)) {
+            return res.status(400).json({ error: "Invalid status Type : " + status });
+        }
+
+        // check if there is already a connection request between the two users
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                { formUserId: fromUserId, toUserId: toUserId },
+                { formUserId: toUserId, toUserId: fromUserId }
+            ]
+        });
+
+        if (existingConnectionRequest) {
+            return res.status(400).json({ error: "Connection request already exists between these users" });
+        }
+
+        //verify if the user exists
+        const toUser = await User.findById(toUserId);
+
+        if (!toUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+
+        const connectionRequest = new ConnectionRequest({
+            formUserId: fromUserId,
+            toUserId: toUserId,
+            status: status
+        });
+
+        const data = await connectionRequest.save(); // Save the connection request to the database
+
+        if(status === 'interested') {
+            // If the status is 'interested', update the user's interested status
+        res.status(201).json({ message: "Connection request sent successfully" ,
+        data: data }
+        );}else{
+            res.status(201).json({ message: "Ignored the profile successfully",
+            data: data 
+             });
+        }
+
+    }catch(err){
+        console.error("Error in sending connection request:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 
-module.exports = requestgRouter;
+
+module.exports = requestRouter;
